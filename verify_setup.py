225b8baa99f8f2347f4dd60bcbab5aabe9f5418f
NYokan <matias.trabajo28524@gmail.com>
2026-06-01 16:52:43 -0400

    chore: inicialización del entorno local y estructura base para BreastMNIST

---
+++ verify_setup.py
@@ -0,0 +1,11 @@
+﻿import medmnist
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from medmnist import INFO, Evaluator
+
+print('MedMNIST version:', medmnist.__version__)
+print('TensorFlow version:', tf.__version__)
+print('Numpy version:', np.__version__)
+print('Setup verified!')
